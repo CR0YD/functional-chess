488cd8f8f4bfc7e391ae1edab8572291a03d12f4
CR0YD <98400750+CR0YD@users.noreply.github.com>
2024-01-25 20:39:11 +0100

    Preparing a new basic project

---
+++ src/main/java/com/croyd/functional_chess/App.java
@@ -0,0 +1,13 @@
+package com.croyd.functional_chess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
